67e7a48146a23019c198e52deee5622c406bc61b
Łukasz Łopata <lukasz.lopata96@gmail.com>
2020-12-12 19:25:12 +0100

    Lab 4 completed

---
+++ Laboratories/ViewComponents/LatestItem.cs
@@ -0,0 +1,26 @@
+﻿using Laboratories.Database;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratories.ViewComponents
+{
+    public class LatestItem : ViewComponent
+    {
+        private readonly ExchangesDbContext _dbContext;
+
+        public LatestItem(ExchangesDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var latestItem = _dbContext.Items.OrderByDescending(X => X.Id).First();
+
+            return View("Index", latestItem);
+        }
+    }
+}
